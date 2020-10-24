016f1d85fecccac6517c998834a2708f2a2c0380
Arthur Santana <43881656+arthurk88@users.noreply.github.com>
2020-10-23 23:55:26 -0300

    Update 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
 
+import LogoImg from './imagens/Logo.svg';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Word! #NLW</h1>
+    <div id="page-landing">
+      <div className='content-wrapper'>
+        <img src={LogoImg} alt='Happy'/>
+
+        <main>
+          <h1>Leve felicidades para o mundo!</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className='location'>
+          <strong>Santo António de Jesus</strong>
+          <span>Bahia</span>
+        </div>
+
+        <a href="#" className='enter-app'>
+          {'->'}
+        </a>
+      </div>
     </div>
   );
 }
